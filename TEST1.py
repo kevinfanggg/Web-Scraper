1eb8ee4dff7c05c516e2489ecaa567a342c385bf
Kevin Fang <156724436+kevinfanggg@users.noreply.github.com>
2025-03-07 10:53:52 -0500

    Update TEST1.py

--- TEST1.py
+++ TEST1.py
@@ -3,7 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-# Connect to the already open Chrome browser
 # Get user input for the product URL
 product_url = input("Enter Walmart Canada Product URL: ").strip()
 
